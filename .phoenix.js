09d3c693afad022f7e217c36dddf78ccdf0d95c0
Mike Larsson <mikelarssonftw@gmail.com>
2017-08-25 18:27:34 -0400

    clean up phoenix.js

--- .phoenix.js
+++ .phoenix.js
@@ -1,33 +1,19 @@
 #!/usr/bin/env coffee -p
 
-Key.on 'left', [ 'ctrl', 'cmd' ], ->
-  screen = Screen.main().flippedVisibleFrame()
-  window = Window.focused()
+arrange = (xp, yp, wp, hp) ->
+  ->
+    screen = Screen.main().flippedVisibleFrame()
+    window = Window.focused()
 
-  if window
-    window.setTopLeft x: 0, y: 0
-    window.setSize width: screen.width / 2, height: screen.height
+    if window
+      window.setTopLeft x: screen.width * xp, y: screen.height * yp
+      window.setSize width: screen.width * wp, height: screen.height * hp
 
-Key.on 'right', [ 'ctrl', 'cmd' ], ->
-  screen = Screen.main().flippedVisibleFrame()
-  window = Window.focused()
 
-  if window
-    window.setTopLeft x: screen.width / 2, y: 0
-    window.setSize width: screen.width / 2, height: screen.height
+Key.on 'left',  [ 'ctrl', 'cmd' ], arrange 0, 0, .5, 1
+Key.on 'right', [ 'ctrl', 'cmd' ], arrange .5, 0, .5, 1
+Key.on 'up',    [ 'ctrl', 'cmd' ], arrange 0, 0, 1, 1
+Key.on 'down',  [ 'ctrl', 'cmd' ], arrange 1/8, 1/8, 6/8, 6/8
 
-Key.on 'up', [ 'ctrl', 'cmd' ], ->
-  screen = Screen.main().flippedVisibleFrame()
-  window = Window.focused()
-
-  if window
-    window.setTopLeft x: 0, y: 0
-    window.setSize width: screen.width, height: screen.height
-
-Key.on 'down', [ 'ctrl', 'cmd' ], ->
-  screen = Screen.main().flippedVisibleFrame()
-  window = Window.focused()
-
-  if window
-    window.setTopLeft x: screen.width / 8, y: screen.height / 8 
-    window.setSize width: screen.width * (6/8), height: screen.height * (6/8)
+Key.on 'left',  [ 'ctrl', 'cmd', 'shift' ], arrange 0, 0, 0.25, 1
+Key.on 'right',  [ 'ctrl', 'cmd', 'shift' ], arrange .75, 0, 0.25, 1
