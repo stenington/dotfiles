1081e99f2bcfdeaafc22acc41fb23966a7ba909d
Mike Larsson <mikelarssonftw@gmail.com>
2017-08-28 14:01:57 -0400

    add send commands to phoenix

--- .phoenix.js
+++ .phoenix.js
@@ -2,13 +2,23 @@
 
 arrange = (xp, yp, wp, hp) ->
   ->
-    screen = Screen.main().flippedVisibleFrame()
     window = Window.focused()
 
     if window
-      window.setTopLeft x: screen.width * xp, y: screen.height * yp
-      window.setSize width: screen.width * wp, height: screen.height * hp
+      frame = window.screen().flippedVisibleFrame()
+      window.setTopLeft x: frame.x + frame.width * xp, y: frame.y + frame.height * yp
+      window.setSize width: frame.width * wp, height: frame.height * hp
 
+
+send = (idx) ->
+  ->
+    window = Window.focused()
+    if window
+      screens = Screen.all()
+      if screens.length >= idx+1
+        screen = screens[idx]
+        window.setFrame screen.flippedVisibleFrame()
+        window.maximize()
 
 Key.on 'left',  [ 'ctrl', 'cmd' ], arrange 0, 0, .5, 1
 Key.on 'right', [ 'ctrl', 'cmd' ], arrange .5, 0, .5, 1
@@ -17,3 +27,6 @@ Key.on 'down',  [ 'ctrl', 'cmd' ], arrange 1/8, 1/8, 6/8, 6/8
 
 Key.on 'left',  [ 'ctrl', 'cmd', 'shift' ], arrange 0, 0, 0.25, 1
 Key.on 'right',  [ 'ctrl', 'cmd', 'shift' ], arrange .75, 0, 0.25, 1
+
+Key.on '1', [ 'ctrl', 'cmd' ], send 0
+Key.on '2', [ 'ctrl', 'cmd' ], send 1
